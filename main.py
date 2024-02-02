5f50ff7d0f95b20ecc0868b469cb0719ae61029a
PushTheCode <43913828+push-the-code@users.noreply.github.com>
2024-02-03 00:44:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,32 +2,32 @@ item_list = {}
 shop_list = []
 
 def shopping_list ():
-    cart = int(input("Enter how many items do you want to add to your cart:"))
+    cart = int(input("Enter how many items do you want to add to your shopping cart:"))
     print("Enter %d items in your cart"%cart, end="\n")
     for i in range(0, cart):
         key = input("\nEnter item number %d:"%(i+1))
         val = input("Enter how much quantity:")
         item_list = {key:val}
         shop_list.append(item_list)
-    print("\n________________________________________")
+
+    print("_"*50)
     print("\n\tYour Shopping List\n\n")
-    print("No. \t Items \t \t Quantity\n")
+    print("No. \t Items \t\t    Quantity\n")
     for i, item_list in enumerate(shop_list):
         for key, value in item_list.items():
-            print("%d " %(i+1), "\t %s\t" %key, "\t %s   " %value)
+            print("%d " %(i+1), "\t %s " %key, "\t\t%s" %value)
         print()
-    print("\n________________________________________")
+    print("_"*50)
 
     
 def main():
     while True:
         shopping_list()
-        ch = input("Do you want add more items?")
+        ch = input("\nDo you want add more items?")
         if ch == "Yes" or ch == "yes":
             shopping_list()
             break
         else:
-            exit(0)
             break
     
 main()
